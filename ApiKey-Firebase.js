2c2685d79441110d7e15a2b0e192eb39400843d9
Melecio <albertomatosgil@gmail.com>
2025-09-12 22:21:29 -0400

    Subida segura sin token expuesto

---
+++ ApiKey-Firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB_Rc2wPwhqMzCQUzDexn4LpGTtr6LWZAs",
+    authDomain: "melangie-42568.firebaseapp.com",
+    projectId: "melangie-42568",
+    storageBucket: "melangie-42568.firebasestorage.app",
+    messagingSenderId: "109387978959",
+    appId: "1:109387978959:web:2eaa19b9cbae0cd310ffba"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
